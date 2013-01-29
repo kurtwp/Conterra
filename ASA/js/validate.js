2c71a3e3ea16bef75e0522ea2ac594395b8cc8b0
Kurt Pasewaldt <kurt@pasewaldt.com>
2013-01-29 18:38:06 -0500

    updated functions

--- ASA/js/validate.js
+++ ASA/js/validate.js
@@ -2,6 +2,8 @@
 function validate() {
 	
 	var regName = document.form.regionName;
+	var mapNumb = document.form.mapNumber;
+	
 	//return validateRegionName(regName);
 
 	function validateRegionName(regName) {
@@ -13,6 +15,19 @@ function validate() {
 			return false;
 		}
 	}
+	
+	function validateMapNumber(mapNumb) {
+		var map_len = mapNumb.value.length;
+		if (map_len != 0) {
+			return true;
+		} else {
+			alert("Enter VLAN number");
+			return false;
+		}
+	}
+	
+	return validateMapNumber(mapNumb);
 	return validateRegionName(regName);
+	
 // End validate function
 }

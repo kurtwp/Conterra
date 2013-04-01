b3145b84b1c22ffa375ce9e0c3b25a7bf10adac5
Kurt Pasewaldt <kurt@pasewaldt.com>
2013-04-01 19:15:21 -0400

    Update form css

--- Ocular/paa.php
+++ Ocular/paa.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../header.html';
 $fldNmbers = ""; //Hold for field numbers
 $fail = "";
 function validateFields($field) {
@@ -7,7 +8,7 @@ function validateFields($field) {
         $arrayCount = count($tempFields);
          for ($i=0; $i<$arrayCount;$i++)
             {
-              echo $tempFields[$i];
+              echo $tempFields[$i] . ",";
             }
             echo "<br />";
         return "";
@@ -26,6 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     } 
     $fail = validateFields($fldNmbers);
     if ($fail == "") {
+        echo $_FILES['paa']['name'] . "<br />";
         move_uploaded_file($_FILES['paa']['tmp_name'], 'C:\wamp\tmp\paa.cvs.gz');
         $lines = gzfile('C:\wamp\tmp\paa.cvs.gz');
         $linesCount = count($lines);
@@ -46,16 +48,21 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     }
 }
 echo <<<_END
+<div id="container">
 <p>$fail</p>
-<form action="paa.php" method="post" enctype="multipart/form-data">
+<form id="contactform" action="paa.php" method="post" enctype="multipart/form-data">
+<h3>Ocular PAA/Regulator Check</h3>
     <div class='field'>
         <label for='insideNetwork'>Enter Field Numbers: </label>
         <input type='text' class='input' size="20" maxlength='150' name='fldNmbers' value='$fldNmbers'/>
         <p class='hint'>To enter in multiple field numbers use "," in between:<br />
         e.g. - 20,21,22,23,24</p>
-    </div>
-    <input type="file" name="paa" />
-    <input type='submit' class='button' value='submit' />
+    </div> <br /><br />
+    <label>Input file: </label>
+   <input type="file" name="paa" />
+   <input type='submit' class='sbutton' value='submit' /> 
 </form>
+</div>
 _END;
-
+require_once '../footer.html';
+?>

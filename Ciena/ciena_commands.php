1b3f19d260d5c85ac28bd5e01a2061367ffa9af8
Kurt Pasewaldt <kurt@pasewaldt.com>
2013-04-15 19:32:27 -0400

    Start Ciena Command page

---
+++ Ciena/ciena_commands.php
@@ -0,0 +1,52 @@
+<?php require_once '../header.html'; ?>
+
+<div id="right_box">
+    <h4>Save Configuration</h4>
+    <ul class="ciena">
+        <li>configuration save</li>
+    </ul>
+    <br />
+    <h4>Add Port discripition</h4>
+    <ul class="ciena">
+        <li>port set port 1 description "Data to MR01"</li>
+    </ul>
+    <br />
+    <h4>Enable Port</h4>
+    <ul class="ciena">
+        <li>port enable port "port number" </li>
+    </ul>
+    <br />
+    <h4>Disable Port</h4>
+    <ul class="ciena">
+        <li>port disable port "port number" </li>
+    </ul>
+    <br />
+    <h4>Set Port MTU to 9216</h4>
+    <ul class="ciena">
+        <li>port set port "port number" max-frame-size 9216</li>
+    </ul>
+    <br />
+    <h4>Create a VLAN</h4>
+    <ul class="ciena">
+        <li>vlan create vlan "vlan ID"</li>
+        <li>vlan create vlan 2,1033-1035,2001</li>
+    </ul>
+    <br />
+    <h4>Add VLANs to a port.</h4>
+    <ul class="ciena">
+        <li>vlan add vlan "vlan ID" port "port number"</li>
+        <li>port set port 1033-1035,2001 port 2,6-11</li>
+    </ul>
+    <br />
+    <h4>Remove VLANs from port</h4>
+    <ul class="ciena">
+        <li>vlan remove vlan "vlan ID" port "port number"</li>
+        <li>vlan remove vlan 1033-1035 port 2</li>
+    </ul>
+</div>
+
+</div>
+
+
+</body>
+</html>

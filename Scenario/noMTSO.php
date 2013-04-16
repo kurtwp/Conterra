5647dc8a993e66233ace21726b08d3b14e19ba85
Kurt Pasewaldt <kurt@pasewaldt.com>
2013-04-16 19:29:30 -0400

    Create Scenario pages

---
+++ Scenario/noMTSO.php
@@ -0,0 +1,36 @@
+<?php require_once '../header.html'; ?>
+
+<div id="right_box">
+    <h3 class="center">A-Side to Z-Side no MTSO</h3>
+    <img src="../../images/SingleHopNoMTSO.png"> <br />
+    <div class="leftcols">
+        <h4>A-Side MetroNID</h4>
+        <ul class="nid">
+            <li>Create One Regulator</li>
+            <li>Create VID Set</li>
+            <li>Create One Policy using VID Set</li>
+            <li>Create Even PAA</li>
+            <li>Create Odd PAA</li>
+        </ul>
+    </div> <!-- end of leftcols -->
+    <div class="rightcols">
+        <h4>Z-Side EtherNID</h4>
+        <ul class="nid">
+            <li>Create One Regulator</li>
+            <li>Create Two VLAN Filters</li>
+            <li>Create Two Policies</li>
+            <li>Create Even PAA</li>
+            <li>Create Odd PAA</li>
+        </ul>
+    </div> <!-- End right_box -->
+    <div class="rightcols">
+        <h4>Z-Side MetroNID</h4>
+        <ul class="nid">
+            <li>Create One Regulator</li>
+            <li>Create VID Set</li>
+            <li>Create One Policy using VID Set</li>
+            <li>Create Even PAA</li>
+            <li>Create Odd PAA</li>
+        </ul>
+    </div> <!-- End of rightcols -->
+</div> <!-- End right_box -->
---
+++ Scenario/withMTSO.php
@@ -0,0 +1,43 @@
+<?php require_once '../header.html'; ?>
+
+<div id="right_box">
+    <h3 class="center">A-Side to Z-Side with MTSO</h3>
+    <img src="../../images/SingleHopWithMTSO.png"> <br />
+    <div class="leftcol">
+        <h4>A-Side MetroNID</h4>
+        <ul class="nid">
+            <li>Create Even Regulator on Even MTSO NID</li>
+            <li>Create Odd Regulator on Odd MTSO NID</li>
+            <li>Create an Even VID Set on Even MTSO NID</li>
+            <li>Create an Odd VID Set on Odd MTSO NID</li>
+            <li>Create an Even Policy using VID Set on Even MTSO NID</li>
+            <li>Create an Even Policy using VID Set on Even MTSO NID</li>
+            <li>Create Even PAA on Even MTSO NID</li>
+            <li>Create Odd PAA on Odd MTSO NID</li>
+        </ul>
+    </div> <!-- end of leftcols -->
+    <div class="rightcol">
+        <h4>Z-Side EtherNID</h4>
+        <ul class="nid">
+            <li>Create Even Regulator</li>
+            <li>Create Odd Regulator</li>
+            <li>Create Two VLAN Filters</li>
+            <li>Create Two Policies</li>
+            <li>Create Even PAA</li>
+            <li>Create Odd PAA</li>
+        </ul>
+    </div> <!-- End right_box -->
+    <div class="rightcol">
+        <h4>Z-Side MetroNID</h4>
+        <ul class="nid">
+            <li>Create Even Regulator</li>
+            <li>Create Odd Regulator</li>
+            <li>Create an Even VID Set</li>
+            <li>Create an Odd VID Set</li>
+            <li>Create One Policy using Even VID Set</li>
+            <li>Create One Policy using Odd VID Set</li>
+            <li>Create Even PAA</li>
+            <li>Create Odd PAA</li>
+        </ul>
+    </div> <!-- End of rightcols -->
+</div> <!-- End right_box -->
